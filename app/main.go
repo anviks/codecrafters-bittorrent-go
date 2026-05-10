4452905df14320df191c57db6479c1d487376093
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-05-10 22:42:08 +0300

    Add peer discovery

--- app/main.go
+++ app/main.go
@@ -6,7 +6,10 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/codecrafters-io/bittorrent-starter-go/app/bencode"
@@ -17,9 +20,6 @@ import (
 var _ = json.Marshal
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
-
 	command := os.Args[1]
 
 	switch command {
@@ -34,7 +34,7 @@ func main() {
 
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
-	case "info":
+	case "info", "peers":
 		torrentPath := os.Args[2]
 		file, _ := os.OpenFile(torrentPath, os.O_RDONLY, 0777)
 		decoded, _ := bencode.Decode(bufio.NewReader(file))
@@ -48,7 +48,26 @@ func main() {
 			pieceHashes.WriteString("\n")
 			pieceHashes.WriteString(hex.EncodeToString([]byte(info["pieces"].(string)[i : i+20])))
 		}
-		fmt.Printf("Tracker URL: %s\nLength: %d\nInfo Hash: %s\nPiece Length: %d\nPiece Hashes: %s\n", data["announce"], info["length"], hex.EncodeToString(hash[:]), info["piece length"], pieceHashes.String())
+		if command == "info" {
+			fmt.Printf("Tracker URL: %s\nLength: %d\nInfo Hash: %s\nPiece Length: %d\nPiece Hashes: %s\n", data["announce"], info["length"], hex.EncodeToString(hash[:]), info["piece length"], pieceHashes.String())
+		} else {
+			vals := make(url.Values)
+			vals.Add("info_hash", string(hash[:]))
+			vals.Add("peer_id", "idk_some_randomid_ig")
+			vals.Add("port", "6881")
+			vals.Add("uploaded", "0")
+			vals.Add("downloaded", "0")
+			vals.Add("left", strconv.Itoa(info["length"].(int)))
+			vals.Add("compact", "1")
+			resp, _ := http.Get(fmt.Sprintf("%s?%s", data["announce"].(string), vals.Encode()))
+			result, _ := bencode.Decode(bufio.NewReader(resp.Body))
+			peers := []byte(result.(map[string]any)["peers"].(string))
+			var peerStr strings.Builder
+			for i := 0; i < len(peers); i += 6 {
+				fmt.Fprintf(&peerStr, "%d.%d.%d.%d:%d\n", peers[i], peers[i+1], peers[i+2], peers[i+3], int32(peers[i+4])<<8|int32(peers[i+5]))
+			}
+			fmt.Print(peerStr.String())
+		}
 	default:
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
