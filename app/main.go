33bca32658def749938b8417b489d68e257f0c0c
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-05-11 20:16:22 +0300

    Create TorrentFile struct and create reusable functions

--- app/main.go
+++ app/main.go
@@ -20,6 +20,73 @@ import (
 // Ensures gofmt doesn't remove the "os" encoding/json import (feel free to remove this!)
 var _ = json.Marshal
 
+type TorrentFile struct {
+	Announce string
+	Info     TorrentInfo
+	InfoHash []byte
+}
+
+type TorrentInfo struct {
+	Name        string
+	Length      int
+	PieceLength int
+	Pieces      string
+}
+
+func parseTorrentFile(torrentPath string) TorrentFile {
+	file, _ := os.OpenFile(torrentPath, os.O_RDONLY, 0777)
+	decoded, _ := bencode.Decode(bufio.NewReader(file))
+	file.Close()
+	d := decoded.(map[string]any)
+
+	info := d["info"].(map[string]any)
+	encodedInfo, _ := bencode.Encode(info)
+	infoHash := sha1.Sum([]byte(encodedInfo))
+
+	return TorrentFile{
+		Announce: d["announce"].(string),
+		Info: TorrentInfo{
+			Name:        info["name"].(string),
+			Length:      info["length"].(int),
+			PieceLength: info["piece length"].(int),
+			Pieces:      info["pieces"].(string),
+		},
+		InfoHash: infoHash[:],
+	}
+}
+
+func findPeers(torrent TorrentFile) []string {
+	vals := make(url.Values)
+	vals.Add("info_hash", string(torrent.InfoHash))
+	vals.Add("peer_id", "idk_some_randomid_ig")
+	vals.Add("port", "6881")
+	vals.Add("uploaded", "0")
+	vals.Add("downloaded", "0")
+	vals.Add("left", strconv.Itoa(torrent.Info.Length))
+	vals.Add("compact", "1")
+
+	resp, _ := http.Get(fmt.Sprintf("%s?%s", torrent.Announce, vals.Encode()))
+	result, _ := bencode.Decode(bufio.NewReader(resp.Body))
+	peers := []byte(result.(map[string]any)["peers"].(string))
+
+	var peerStr []string
+	for i := 0; i < len(peers); i += 6 {
+		peerStr = append(peerStr, fmt.Sprintf("%d.%d.%d.%d:%d", peers[i], peers[i+1], peers[i+2], peers[i+3], int32(peers[i+4])<<8|int32(peers[i+5])))
+	}
+
+	return peerStr
+}
+
+func performHandshake(connection net.Conn, infoHash []byte) {
+	message := make([]byte, 68)
+	message[0] = 19
+	copy(message[1:20], []byte("BitTorrent protocol"))
+	copy(message[20:28], []byte{0, 0, 0, 0, 0, 0, 0, 0})
+	copy(message[28:48], infoHash)
+	copy(message[48:68], []byte("idk_some_randomid_ig"))
+	connection.Write(message)
+}
+
 func main() {
 	command := os.Args[1]
 
@@ -36,49 +103,21 @@ func main() {
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
 	case "info", "peers", "handshake":
-		torrentPath := os.Args[2]
-		file, _ := os.OpenFile(torrentPath, os.O_RDONLY, 0777)
-		decoded, _ := bencode.Decode(bufio.NewReader(file))
-		file.Close()
-		data := decoded.(map[string]any)
-		info := data["info"].(map[string]any)
-		encoded_info, _ := bencode.Encode(info)
-		hash := sha1.Sum([]byte(encoded_info))
+		torrent := parseTorrentFile(os.Args[2])
 		var pieceHashes strings.Builder
-		for i := 0; i < len(info["pieces"].(string)); i += 20 {
+		for i := 0; i < len(torrent.Info.Pieces); i += 20 {
 			pieceHashes.WriteString("\n")
-			pieceHashes.WriteString(hex.EncodeToString([]byte(info["pieces"].(string)[i : i+20])))
+			pieceHashes.WriteString(hex.EncodeToString([]byte(torrent.Info.Pieces[i : i+20])))
 		}
 
 		switch command {
 		case "info":
-			fmt.Printf("Tracker URL: %s\nLength: %d\nInfo Hash: %s\nPiece Length: %d\nPiece Hashes: %s\n", data["announce"], info["length"], hex.EncodeToString(hash[:]), info["piece length"], pieceHashes.String())
+			fmt.Printf("Tracker URL: %s\nLength: %d\nInfo Hash: %s\nPiece Length: %d\nPiece Hashes: %s\n", torrent.Announce, torrent.Info.Length, hex.EncodeToString(torrent.InfoHash), torrent.Info.PieceLength, pieceHashes.String())
 		case "peers":
-			vals := make(url.Values)
-			vals.Add("info_hash", string(hash[:]))
-			vals.Add("peer_id", "idk_some_randomid_ig")
-			vals.Add("port", "6881")
-			vals.Add("uploaded", "0")
-			vals.Add("downloaded", "0")
-			vals.Add("left", strconv.Itoa(info["length"].(int)))
-			vals.Add("compact", "1")
-			resp, _ := http.Get(fmt.Sprintf("%s?%s", data["announce"].(string), vals.Encode()))
-			result, _ := bencode.Decode(bufio.NewReader(resp.Body))
-			peers := []byte(result.(map[string]any)["peers"].(string))
-			var peerStr strings.Builder
-			for i := 0; i < len(peers); i += 6 {
-				fmt.Fprintf(&peerStr, "%d.%d.%d.%d:%d\n", peers[i], peers[i+1], peers[i+2], peers[i+3], int32(peers[i+4])<<8|int32(peers[i+5]))
-			}
-			fmt.Print(peerStr.String())
+			fmt.Println(strings.Join(findPeers(torrent), "\n"))
 		case "handshake":
 			conn, _ := net.Dial("tcp", os.Args[3])
-			message := make([]byte, 68)
-			message[0] = 19
-			copy(message[1:20], []byte("BitTorrent protocol"))
-			copy(message[20:28], []byte{0, 0, 0, 0, 0, 0, 0, 0})
-			copy(message[28:48], hash[:])
-			copy(message[48:68], []byte("idk_some_randomid_ig"))
-			conn.Write(message[:])
+			performHandshake(conn, torrent.InfoHash)
 			buf := make([]byte, 68)
 			conn.Read(buf)
 			peerId := buf[48:68]
