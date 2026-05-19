2a60b0f52751e9d7625081a2e148816c52f0c140
Andreas Viks <117015417+anviks@users.noreply.github.com>
2026-05-19 21:39:43 +0300

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -45,6 +45,8 @@ type ConnectionInfo struct {
 func parseTorrentFile(torrentPath string) TorrentFile {
 	file, _ := os.OpenFile(torrentPath, os.O_RDONLY, 0777)
 	decoded, _ := bencode.Decode(bufio.NewReader(file))
+	jsonOutput, _ := json.Marshal(decoded)
+	fmt.Println(string(jsonOutput))
 	file.Close()
 	d := decoded.(map[string]any)
 
